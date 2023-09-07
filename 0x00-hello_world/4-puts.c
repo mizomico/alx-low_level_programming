aa2e190a39a1308906d0aff2a09aee7e09bacc6d
mizomico <aroky.me@gmail.com>
2023-09-07 16:02:34 -0700

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Description: A C Program that prints with put function 
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
